636f1bb52a3f57e53f7bb1bbf5a24fd0f00a2ce0
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:48:05 -0800

    working dictionary sort

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,33 +1,37 @@
 # your code goes here
 import sys
 
-#filename = "scores.txt" sys.argv[1]
-
 def convert_text_to_dict(filename):
     """converting scores.txt into restaurant rating 
     dictionary
     """
-
+    #create empty dictionary
     restaurant_rating = {}
 
+    #open file and loop through data to clean and split data
     for scores_data in open(filename):
         scores_data = scores_data.rstrip().split(":")
         restaurant, rating = scores_data
-        # print restaurant, rating
         
+        #checks dictionary for entry, adds restaurant and rating if not already in
+        #dictionary
+
         if restaurant not in restaurant_rating:
-            restaurant_rating[restaurant] = int(scores_data[1])
+            restaurant_rating[restaurant] = int(rating)
            
     return restaurant_rating
 
-restaurant_rating = convert_text_to_dict("scores.txt")
 
 def sort_restaurant_dict(restaurant_rating):
-    """doc string here"""
+    """sorts restaurant_rating dict and outputs in alphabetized by restaurant string"""
 
+    #create list of tuples of restaurant and rating
+    #and returns sorted list by restaurant name
     restaurant_list = sorted(restaurant_rating.items())
     
+    #loops through sorted restaurant_list to print string
     for restaurant, rating in restaurant_list:
-        print " %s is rated at %d" % (restaurant,rating)
+        print " %s is rated at %d." % (restaurant,rating)
 
+restaurant_rating = convert_text_to_dict("scores.txt")
 sort_restaurant_dict(restaurant_rating)
